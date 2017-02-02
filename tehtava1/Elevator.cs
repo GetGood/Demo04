460b73b644e92deda37c218205e378302662d685
K2851 <K2851@OHJE-330-WS-15>
2017-02-02 10:33:04 +0200

    teht1 aloitettu

---
+++ tehtava1/Elevator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava1
+{
+    class Elevator
+    {
+        private readonly int maxFloor = 6, minFloor = 1;
+
+        private int floor;
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value <= maxFloor && value >= minFloor) floor = value;
+                else
+                {
+                    Console.WriteLine("No more rooms");
+                    floor = maxFloor;
+                }
+            }
+        }
+    }
+}
