4d853e47fc426296d3db7bc18404cfc1029dfea0
k2851 <k2851@DESKTOP-9J2R0SN>
2017-02-07 12:57:49 +0200

    hm

---
+++ tehtava5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            radio.PrintVolume();
+            radio.PrintFreguency();
+            radio.Volume = 6;
+            radio.PrintVolume();
+            Console.WriteLine("Type 1 to adjust volume, 2 to adjust freguency 0 to turn on/off, 4 to exit");
+            string line = Console.ReadLine();
+            int choice;
+            bool result = int.TryParse(line, out choice);
+
+                switch (choice)
+                {
+                    case 2:
+
+                        while (true)
+                        {
+                            Console.Write("give a new freguency (2000-26 000) type 0 to exit > ");
+                            string line2 = Console.ReadLine();
+                            double number;
+                            bool result2 = double.TryParse(line2, out number);
+                            if (number == 0)
+                            {
+                                break;
+                            }
+                            radio.Freg = number;
+                            radio.PrintFreguency();
+
+                        }
+                        break;
+                }
+            
+            
+   
+        Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ tehtava5/Radio.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava5
+{
+    class Radio
+    {
+        private readonly int maxVolume = 9, minVolume = 1;
+        private readonly double maxFreg = 26000, minFreg = 2000;
+        private int volume = 1;
+        private double freg;
+        public bool GearWheels { get; set; }
+        public void PrintVolume()
+        {
+            Console.WriteLine("The volume is set to " + Volume);
+        }
+        public void PrintFreguency()
+        {
+            Console.WriteLine("The freguency is set to " + Freg);
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value >= maxVolume)
+                {
+                    Console.WriteLine("max volume 9");
+                    volume = maxVolume;
+                }
+                else if (value <= minVolume)
+                {
+                    Console.WriteLine("min volume 1");
+                    volume = minVolume;
+                }
+                else
+                {
+                    volume = value;
+                }
+
+
+            }
+
+        }
+        public double Freg
+        {
+            get
+            {
+                return freg;
+            }
+            set
+            {
+                if (value >= maxFreg)
+                {
+                    Console.WriteLine("max freguency 26 000");
+                    freg = maxFreg;
+                }
+                else if (value <= minFreg)
+                {
+                    Console.WriteLine("min freguency 2000");
+                    freg = minFreg;
+                }
+                else
+                {
+                    freg = value;
+                }
+
+
+            }
+
+        }
+
+
+    }
+}
